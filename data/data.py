effcd13cf53ccf6397f7f54c44d49ba6b01a94ac
Peiwu Qin <qaydm1979@gmail.com>
2016-12-10 23:43:17 -0700

    data hash check.

---
+++ data/data.py
@@ -0,0 +1,25 @@
+"""data.py
+
+Simple script to validate the downloaded with
+with the hashes in a .json file.
+
+Prerequesite :
+   To create the json file relative to a specific
+   folder, see project-epsilon/data/get_data_hashes.py
+
+Assuming the ds005_hashes.json file is present
+in the directory:
+   python data.py
+"""
+
+import pdb
+import json
+
+from data_hashes import check_hashes
+
+if __name__ == "__main__":
+    file_ls = ['sub_data_hashes.json']
+    for f in file_ls:
+        with open(f) as infile:
+            f_dict = json.load(infile)
+            check_hashes(f_dict)
---
+++ data/get_data_hashes.py
@@ -0,0 +1,24 @@
+"""get_data_hashes.py
+
+Creates a .json file with hashes for the testing data ds114
+and the filtered data ds005_filtered containing file names
+and corresponding hashes.
+Input the directories paths in dirs_ls
+
+Run with:
+   python get_data_hashes.py
+   from the directory relative to the input paths
+
+"""
+from __future__ import print_function
+from data_hashes import *
+import json
+import pdb
+
+if __name__ == "__main__":
+    dirs_ls = ['sub_data']
+    for d in dirs_ls:
+        d_dict = generate_dir_md5(d)
+        with open(d + '_hashes.json', 'w') as file_out:
+
+            json.dump(d_dict, file_out)
